9fcc92101ba372e4c713982042f532dfd277a436
shimanamisan <mikan.sup.all@gmail.com>
2023-09-06 08:26:39 +0900

    udpate: 修正

--- public/index.php
+++ public/index.php
@@ -1,3 +1,20 @@
 <?php
 
-echo 'hello world!';
+printf("Hello, %s\n", "world");
+
+// stdoutに対するファイルポインタを取得
+$stdout = fopen('php://stdout', 'w');
+
+// ファイルポインタに対して書き込む（これが内部のstdoutバッファに書き込まれる）
+fwrite($stdout, "Hello, World!\n");
+
+// stdoutバッファをフラッシュ（即座に出力）
+fflush($stdout);
+
+// ファイルポインタを閉じる
+fclose($stdout);
+
+
+echo "What is your favorite food??\n";
+$food = fgets(STDIN);
+echo "Thanks for letting me know your favorite food is " . trim($food) . "\n";
